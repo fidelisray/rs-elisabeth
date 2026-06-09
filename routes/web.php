254ad798d00592c2539c0c11231d8aeb30d255a7
fidelisray <arjefidelisraymond@gmail.com>
2026-06-09 13:55:58 +0700

    feat(route): membuat routes untuk setiap halaman / hospital API pages

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\StaffController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -12,3 +14,16 @@ Route::get('/', function () {
 Route::get('/dokter', function() {
     return view('dokter');
 });
+
+// Beranda
+Route::get('/', [BerandaController::class, 'index'])->name('beranda');
+
+// Halaman Dokter
+Route::prefix('dokter')->name('dokter.')->group(function () {
+    Route::get('/',       [DokterController::class, 'index'])->name('index');
+    Route::get('/{id}',   [DokterController::class, 'detail'])->name('detail')
+         ->where('id', '[0-9]+');
+});
+
+// Halmaan Staff
+Route::get('/staff', [StaffController::class, 'index'])->name('staff');
