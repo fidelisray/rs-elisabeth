b1dd576f669da3fc2ad718e5e8999916187a5e89
fidelisray <arjefidelisraymond@gmail.com>
2026-06-10 14:11:46 +0700

    chore(provider): register API Service in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Services\DoctorApiService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +13,9 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        $this->app->singleton(DoctorApiService::class, function ($app) {
+        return new DoctorApiService();
+    });
     }
 
     /**
