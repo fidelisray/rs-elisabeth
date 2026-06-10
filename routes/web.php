88131062b3e9409ae461deac2c20d9d6d038f53e
fidelisray <arjefidelisraymond@gmail.com>
2026-06-10 14:14:54 +0700

    feat(route): membuat routes untuk halaman dokter

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DokterController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -9,6 +10,13 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/dokter', function() {
-    return view('dokter');
+// Route::get('/dokter', function() {
+//     return view('dokter');
+// });
+
+// Halaman Dokter
+Route::prefix('dokter')->name('dokter.')->group(function () {
+    Route::get('/',       [DokterController::class, 'index'])->name('index');
+    Route::get('/{id}',   [DokterController::class, 'detail'])->name('detail')
+         ->where('id', '[0-9]+');
 });
