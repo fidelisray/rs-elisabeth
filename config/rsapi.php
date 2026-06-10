1233262558bef6f827bcc75b69a7edcf310b7c86
fidelisray <arjefidelisraymond@gmail.com>
2026-06-10 14:06:55 +0700

    chore(config): menambahkan file konfigurasi API rsapi

---
+++ config/rsapi.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'base_url' => env('RS_API_BASE_URL'),
+    'api_key' => env('RS_API_KEY'),
+    'timeout' => env('RS_API_TIMEOUT'),
+    'medin_endpoint' => env('RS_MEDIN_ENDPOINT'),
+
+    // Kredensial untuk generate token
+    'auth' => [
+        'endpoint' => env('RS_API_AUTH_ENDPOINT'),
+        'username' => env('RS_API_USERNAME'),
+        'password' => env('RS_API_PASSWORD'),
+    ],
+
+    'token_ttl_buffer' => env('RS_API_TOKEN_BUFFER', 60),
+
+    'cache_ttl' => [
+        'dokter' => 3600,
+        'staff' => 3600,
+        'jadwal' => 1800,
+        'klinik' => 1800,
+    ],
+];
