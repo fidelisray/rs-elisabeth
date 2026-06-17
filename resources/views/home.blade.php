a35e7f10ffb33161ea661b0a50eba1bb3bf31bf4
fidelisray <arjefidelisraymond@gmail.com>
2026-06-17 14:28:54 +0700

    work in progress: linked css file with vite

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,10 @@
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <title>RS St. Elisabeth</title>
-      @vite {[
-        'resources/js/app.js',
-        'resources/js/bootstrap.js',
-        'resources/css/style.css',
-        'resources/css/dokter.css']
-      }
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+      @vite([
+        'resources/css/style.css',
+      ])
   </head>
   <body>
     <header>
@@ -382,7 +379,7 @@
         <div class="container py-5">
             <div class="row cards">
                 <div class="col-md-4 mb-4">
-                    <div class="card h-100" style="width: 22rem;">
+                    <div class="card h-100 shadow-sm border-0" style="width: 22rem;">
                         <img src="{{ asset('images/ADS1749518930.jpg') }}" class="card-img" alt="...">
                         <div class="card-body">
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
@@ -390,7 +387,7 @@
                     </div>
                 </div>    
                 <div class="col-md-4 mb-4">
-                    <div class="card h-100" style="width: 22rem;">
+                    <div class="card h-100 shadow-sm border-0" style="width: 22rem;">
                         <img src="{{ asset('images/ADS1749518080.jpeg') }}" class="card-img" alt="...">
                         <div class="card-body">
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
@@ -398,7 +395,7 @@
                     </div>
                 </div>    
                 <div class="col-md-4 mb-4">
-                    <div class="card h-100" style="width: 22rem;">
+                    <div class="card h-100 shadow-sm border-0" style="width: 22rem;">
                         <img src="{{ asset('images/ADS1758074522.jpeg') }}" class="card-img" alt="...">
                         <div class="card-body">
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
@@ -406,7 +403,7 @@
                     </div>
                 </div>    
                 <div class="col-md-4 mb-4">
-                    <div class="card h-100" style="width: 22rem;">
+                    <div class="card h-100 shadow-sm border-0" style="width: 22rem;">
                         <img src="{{ asset('images/ADS1758075145.jpeg') }}" class="card-img" alt="...">
                         <div class="card-body">
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
@@ -414,7 +411,7 @@
                     </div>
                 </div>
                 <div class="col-md-4 mb-4">
-                    <div class="card h-100" style="width: 22rem;">
+                    <div class="card h-100 shadow-sm border-0" style="width: 22rem;">
                         <img src="{{ asset('images/ADS1761805772.jpeg') }}" class="card-img" alt="...">
                         <div class="card-body">
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
@@ -422,7 +419,7 @@
                     </div>
                 </div>
                 <div class="col-md-4 mb-4">
-                    <div class="card h-100" style="width: 22rem;">
+                    <div class="card h-100 shadow-sm border-0" style="width: 22rem;">
                         <img src="{{ asset('images/ADS1761805889.jpeg') }}" class="card-img" alt="...">
                         <div class="card-body">
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
@@ -476,11 +473,5 @@
     </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/726e331ad1.js" crossorigin="anonymous"></script>
-    @vite {
-        'resources/js/app.js',
-        'resources/js/bootstrap.js',
-        'resources/css/style.css',
-        'resources/css/dokter.css',
-      }
   </body>
 </html>
