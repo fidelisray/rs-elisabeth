acb1a3b6cff612705895572b4d9a6a4046747090
fidelisray <arjefidelisraymond@gmail.com>
2026-06-19 10:01:59 +0700

    fix: fix routes from undefined controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BerandaController;
 use App\Http\Controllers\StaffController;
 
 // Route::get('/', function () {
@@ -15,21 +14,8 @@ Route::get('/dokter', function() {
     return view('dokter');
 });
 
-// Beranda
-Route::get('/', [BerandaController::class, 'index'])->name('beranda');
-
-// Halaman Dokter
-Route::prefix('dokter')->name('dokter.')->group(function () {
-    Route::get('/',       [DokterController::class, 'index'])->name('index');
-    Route::get('/{id}',   [DokterController::class, 'detail'])->name('detail')
-         ->where('id', '[0-9]+');
-});
-
 // Halmaan Staff
 Route::get('/staff', [StaffController::class, 'index'])->name('staff');
-
-
-
 
 
 Route::get('/halaman-jadwal', function() {
