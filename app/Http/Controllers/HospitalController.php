c5a71cfd3f81876ef8bd3001925b6389bf964277
fidelisray <arjefidelisraymond@gmail.com>
2026-06-19 13:01:04 +0700

    feat(controller): membuat HospitalController dan menghapus StaffController - Membuat method untuk memanggil request ke API Service - Membuat method untuk menyimpan data image ke dalam storage - Mengubah nama dari StaffController menjadi HospitalController

---
+++ app/Http/Controllers/HospitalController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\HospitalApiService;
+use Illuminate\Support\Facades\Storage;
+
+class HospitalController extends Controller
+{
+    //
+    public function __construct(
+        protected HospitalApiService $apiService
+    ) {}
+
+    public function getPromotions(Request $request)
+    {
+        $category = 'banner';
+
+        $request = $this->apiService->GetPromotionsList($category);
+
+        // $request = response()->json($request);
+
+        // dd($request);
+        $this->savePhoto($request[0]['gambar'], $request[0]['judul']);
+
+        return response()->json($request);
+    }
+
+
+    public function savePhoto(string $base64Image, string $judul): string
+    {
+        // Buang prefix data URI kalau API mengirim dengan prefix
+        // (kadang API mengirim "data:image/jpeg;base64,..." kadang cuma raw base64)
+        // if (str_contains($base64Image, ',')) {
+        //     $base64Image = explode(',', $base64Image)[1];
+        // }
+
+        $imageData = base64_decode($base64Image);
+
+        $filename = "images/promotions/{$judul}.jpg";
+        Storage::disk('public')->put($filename, $imageData);
+
+        dd(Storage::disk('public')->url($filename));
+    }
+    
+}
